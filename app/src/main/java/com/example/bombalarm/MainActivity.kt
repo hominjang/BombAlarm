035f2f4f6fd69e3d1a7823062453036d7aee0d19
hominjang <51106030+hominjang@users.noreply.github.com>
2022-11-09 17:26:53 +0900

    22/11/09 Hard reset + compose applying
    .

--- app/src/main/java/com/example/bombalarm/MainActivity.kt
+++ app/src/main/java/com/example/bombalarm/MainActivity.kt
@@ -1,11 +1,60 @@
 package com.example.bombalarm
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Divider
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Surface
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.BlendMode.Companion.Color
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.bombalarm.ui.theme.BombAlarmTheme
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContent {
+            BombAlarmTheme {
+                // A surface container using the 'background' color from the theme
+                Surface(
+                    modifier = Modifier.fillMaxSize(),
+                    color = MaterialTheme.colors.background
+                ) {
+                    Greeting("Android")
+                    Greeting("Hello")
+                }
+            }
+        }
+    }
+}
+
+@Composable
+fun Greeting(name: String) {
+    Surface(color = MaterialTheme.colors.primary) {
+        Text(text = "Hello $name!")
+    }
+}
+
+@Composable
+fun MyScreenContent() {
+    Column {
+        Greeting("Android")
+        Divider()
+        Greeting("there")
+    }
+}
+
+
+@Preview(showBackground = true)
+@Composable
+fun DefaultPreview() {
+    BombAlarmTheme {
+
+        MyScreenContent()
     }
 }
--- app/src/main/java/com/example/bombalarm/viewmodel/AlarmViewModel.kt
+++
@@ -1,8 +0,0 @@
-package com.example.bombalarm.viewmodel
-
-import androidx.lifecycle.ViewModel
-
-class AlarmViewModel : ViewModel() {
-    // TODO: Implement the ViewModel
-
-}
